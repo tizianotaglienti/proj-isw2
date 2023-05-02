20abdb21ab96a8aac643de22c3563da03393c039
tizianotaglienti <taglienti.t@gmail.com>
2023-05-02 14:12:08 +0200

    jirahelper

--- src/main/java/JiraHelper.java
+++ src/main/java/JiraHelper.java
@@ -1,4 +1,20 @@
 package main.java;
 
+import main.java.entities.Version;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class JiraHelper {
+    private String projectName;
+
+    public JiraHelper(String projectName) {
+        this.projectName = projectName;
+    }
+
+
 }
